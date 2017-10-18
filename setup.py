d5b17646277a5e1ca6861de4112130acd4d54071
Oliver4242 <oliver.duerr@gmail.com>
2017-10-18 21:04:09 +0200

    First Skeleton for the packages

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='dl_utils',
+      version='0.1',
+      description='Utilities used in DL',
+      url='http://github.com/tensorchiefs/dl_utils',
+      author='Tensorchiefs',
+      author_email='none@example.com',
+      packages=['dl_utils'],
+      zip_safe=False)
