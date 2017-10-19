addad5506a94e935f9e827657348e14f8e4f0573
Oliver Dürr <oliver.duerr@gmail.com>
2017-10-19 11:18:25 +0200

    Update __init__.py

--- dl_utils/__init__.py
+++ dl_utils/__init__.py
@@ -4,4 +4,4 @@ from .tf import *
 
 def joke():
     return (u'Wenn ist das Nunst\u00fcck git und Slotermeyer? Ja! ... '
-            u'Beiherhund das Oder die Flipperwaldt gersput.')
+            u'Beiherhund das Oder die Flipperwaldt gersput. Test')
